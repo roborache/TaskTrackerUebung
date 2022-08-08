f48b50801f80d6bbf7aaa8ef468711c5c81128f3
Tahir <roborache.ta@gmail.com>
2022-08-08 22:25:14 +0200

    initial commit

---
+++ src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number; //? steht für optionale eingabe
+    text: string;
+    day: string;
+    reminder: boolean;
+  }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[]= [
+    {
+    id: 1,
+    text: 'eren godluk time',
+    day: 'August 8th at 3pm',
+    reminder: true,
+    
+    },
+    {
+        id: 2,
+        text: 'lernen time',
+        day: 'August 11th at 3pm',
+        reminder: true,
+    },
+    {
+            id: 3,
+            text: 'sikmek bigmac time',
+            day: 'August 15th at 3pm',
+            reminder: true,
+    },
+
+];
